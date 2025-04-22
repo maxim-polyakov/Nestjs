660263ddfc5bcc39978029e542d9dce3eff34fdb
maxim-polyakov <maxim7012@gmail.com>
2025-04-22 16:05:55 +0300

    u

--- project/src/users/users.controller.ts
+++ project/src/users/users.controller.ts
@@ -1,4 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Body, Patch, Delete, Query } from '@nestjs/common';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    @Get()
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return [];
+    }
+
+    @Get('interns')
+    findAllInterns() {
+        return [];
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return { id };
+    }
+
+    @Post()
+    create(@Body() user: {}) {
+        return user;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() userUpdate:{}) {
+        return { id, ...userUpdate };
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return { id };
+    }
+
+}
